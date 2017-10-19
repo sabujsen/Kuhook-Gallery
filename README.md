c24f06fed899be4fa226573e0db3742fe9450524
sabujsen <sen.sabuj82@gmail.com>
2017-10-20 03:58:05 +0530

    added & routing

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# KuhookGallery
+# Kuhook-Gallery
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.1.3.
 
@@ -35,10 +35,15 @@ Nanogallery2
 <script  type="text/javascript">src="jquery.nanogallery2.min.js"></script>
 
 ------------------------------------------
-
+# Branches:
+    1.master
+    2.development
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+
+Add Pages
+Add Routing
 
 ## Code scaffolding
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AddImageComponent } from './add-image/add-image.component';
+import { GalleryViewComponent } from './gallery-view/gallery-view.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: GalleryViewComponent },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'gallery-view/:id',
+    component: GalleryViewComponent
+  },
+  { path: 'gallery-view', component: AddImageComponent },
+  { path: 'gallery-view', component: GalleryViewComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddImageComponent,
+    GalleryViewComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     FlexLayoutModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
